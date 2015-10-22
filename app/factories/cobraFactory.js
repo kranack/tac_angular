ac5b04371e9989cc6450a6a1eaabf7bc2f8c40d4
Damien Calesse <madeinjb@gmail.com>
2015-10-22 07:27:06 +0200

    suppression de la factory

--- app/factories/cobraFactory.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var cobraFactory = angular.module('cobraFactory', []);
-
-cobraFactory.factory('connect', function() {
-    return {
-        result: 'connect to Cobra'
-    };
-});
